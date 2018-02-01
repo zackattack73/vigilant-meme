9dad107a88468f0ca4d56932808408fb6288871b
capsukawa <capsukawa@gmail.com>
2018-02-01 12:00:45 +0100

    RMI init

---
+++ Lab-RMI/1_HelloApp/HelloImpl.java
@@ -0,0 +1,16 @@
+
+import java.rmi.*;
+
+public  class HelloImpl implements Hello {
+
+	private String message;
+ 
+	public HelloImpl(String s) {
+		message = s ;
+	}
+
+	public String sayHello() throws RemoteException {
+		return message ;
+	}
+}
+
